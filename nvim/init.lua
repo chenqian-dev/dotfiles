2c8060b8172d8a456f5ff148ecd2fc3e1bda315e
phelps <283861026@qq.com>
2022-11-23 15:35:12 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,14 @@ require "base"
 -- 插件
 require "plugin"
 require "autocmds"
+
+-- WSL yank support
+vim.cmd [[
+let s:clip = '/mnt/c/Windows/System32/clip.exe' 
+if executable(s:clip)
+    augroup WSLYank
+        autocmd!
+        autocmd TextYankPost * if v:event.operator ==# 'y' | call system(s:clip, @0) | endif
+    augroup END
+endif
+]]
